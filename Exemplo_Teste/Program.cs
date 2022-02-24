8426ab12e9b43ad53d428e0cf723bdba18b1a14d
Fabio2222 <fabiomoreira73@outlook.com>
2022-02-24 10:50:49 -0300

    Add project files.

---
+++ Exemplo_Teste/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exemplo_Teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            Console.WriteLine("Digite seu nome: ");
+            nome = Console.ReadLine();
+            Console.WriteLine("Boa tarde: {0}", nome);
+        }
+    }
+}
